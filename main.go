93687cc2f90f0d10778a3077682bf0308df4cd78
yuita-yoshihiko <soup998211@gmail.com>
2023-07-21 23:31:24 +0900

    section 3

--- main.go
+++ main.go
@@ -2,10 +2,55 @@ package main
 
 import (
 	"fmt"
-	"time"
 )
 
+var i5 int = 500
+
+func outer() {
+	var s4 string = "outer"
+	fmt.Println(s4)
+}
+
 func main() {
-	fmt.Println("Hello World")
-	fmt.Println(time.Now())
+
+	var i int = 100
+  fmt.Println(i)
+
+  var s string = "Hello World"
+	fmt.Println(s)
+
+	var t, f bool = true, false
+	fmt.Println(t, f)
+
+	var (
+		i2 int = 200
+		s2 string = "Golang"
+	)
+	fmt.Println(i2, s2)
+
+	var i3 int
+	var s3 string
+	fmt.Println(i3, s3)
+
+	i3 = 300
+	s3 = "Go"
+	fmt.Println(i3, s3)
+
+	i = 150
+	fmt.Println(i)
+
+	i4 := 400
+	fmt.Println(i4)
+
+	i4 = 450
+	fmt.Println(i4)
+
+	fmt.Println(i5)
+
+	outer()
+
+	var s5 string = "Not Use"
+
+	fmt.Println(s5)
+
 }
