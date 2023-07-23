23dd7c1207831ac9026ec19da42d4d61b44000de
yuita-yoshihiko <soup998211@gmail.com>
2023-07-23 17:02:05 +0900

    section5,6,10

---
+++ section6/main.go
@@ -0,0 +1,52 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println(1 + 2)
+	fmt.Println("ABC" + "DE")
+
+	fmt.Println(5 - 1)
+
+	fmt.Println(5 * 4)
+
+	fmt.Println(60 / 3)
+
+	fmt.Println(9 % 4)
+
+	n := 0
+	n += 4
+	fmt.Println(n)
+	n++
+	fmt.Println(n)
+	n--
+	fmt.Println(n)
+
+	s := "ABC"
+	s += "DEF"
+	fmt.Println(s)
+
+
+
+	fmt.Println(1 ==1)
+	fmt.Println(1 == 2)
+	fmt.Println(4 <= 8)
+	fmt.Println(1 >= 8)
+	fmt.Println(1 < 8)
+	fmt.Println(3 > 1)
+
+	fmt.Println(true == false)
+	fmt.Println(true != false)
+	
+
+
+	fmt.Println(true && false == true)
+	fmt.Println(true && true == true)
+	fmt.Println(true && false == false)
+
+	fmt.Println(true || false == true)
+	fmt.Println(false || false == true)
+
+	fmt.Println(!true)
+	fmt.Println(!false)
+}
